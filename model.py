1eaadbd640639643551c74a91a16968e323e95d2
hirish <me@henryirish.com>
2013-10-15 21:26:14 -0700

    Added basic debt model.

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -16,3 +17,30 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Debt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    debtor = db.Column(db.Integer)
+    lender = db.Column(db.Integer)
+
+    # Stored as no. cents
+    amount = db.Column(db.Integer)
+
+    paid = db.Column(db.Boolean)
+
+    def __init__(self, debtor, lender, amount):
+        self.debtor = debtor
+        self.lender = lender
+        self.amount = amount
+        self.paid = False
+
+    def __repr__(self):
+        if self.paid:
+            return "<Debt: %i owed %i %i>" % (self.debtor,
+                                              self.lender,
+                                              self.amount)
+        else:
+            return "<Debt: %i owes %i %i>" % (self.debtor,
+                                              self.lender,
+                                              self.amount)
