23253e0a6d0b24aa739271a2cb6b5e9911e35941
hirish <me@henryirish.com>
2013-10-15 23:14:20 -0700

    Added new user registration.

--- checkdown.py
+++ checkdown.py
@@ -1,3 +1,4 @@
+from flask import request
 from models import User, Debt 
 from config import app, db
 import json
@@ -9,14 +10,32 @@ def index():
 
 @app.route('/user/<user_id>')
 def get_user(user_id):
-  user = User.query.get(user_id)
-  return user.__repr__()
+    user = User.query.get(user_id)
+    return user.__repr__()
 
 @app.route('/user/<user_id>/debt')
 def get_user_debts(user_id):
     debtor = User.query.get(user_id)
     debts = Debt.query.filter_by(debtor=debtor).order_by(Debt.created)
     return debts.stringify
+
+@app.route('/create/user')
+def create_user():
+    username = request.args['username']
+    username = username.strip()
+
+    if len(username) == 0:
+        return "Failed, username empty."
+
+    conflicts = User.query.filter_by(username=username).all()
+    if len(conflicts) > 0:
+        return "Failed, username already taken."
+
+    new_user = User(username, None)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return new_user.json()
 
 
 if __name__ == '__main__':
