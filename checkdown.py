af1602483475e670f227c1b46744716fa21427cf
hirish <me@henryirish.com>
2013-10-15 22:47:41 -0700

    Add User API

--- checkdown.py
+++ checkdown.py
@@ -7,6 +7,11 @@ def index():
     #return 'Us teabagging Billsup.<br /><br/>ASCII-art coming soon.'
     return app.send_static_file('index.html')
 
+@app.route('/user/<user_id>')
+def get_user(user_id):
+  user = User.query.get(user_id)
+  return user.__repr__()
+
 @app.route('/user/<user_id>/debt')
 def get_user_debts(user_id):
     debtor = User.query.get(user_id)
