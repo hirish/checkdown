c8552b46edc7260928361735452a0327f6f733af
hirish <me@henryirish.com>
2013-11-22 01:02:50 -0800

    Added some python code for doing CRUD on users and debts. Needs work before robust and secure

--- checkdown.py
+++ checkdown.py
@@ -1,5 +1,5 @@
 from flask import abort, request, render_template
-from models import User, Debt 
+from models import User, Debt
 from config import app, db
 import json
 
@@ -8,22 +8,131 @@ def index():
     #return 'Us teabagging Billsup.<br /><br/>ASCII-art coming soon.'
     return app.send_static_file('index.html')
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 def get_users():
-    users = User.query.all()
-    return json.dumps([ user.dictify() for user in users ] )
-    # return json.dumps({'users': [ user.dictify() for user in users ] })
+    if request.method == "POST":
+        try:
+            username = request.json['username']
+            email = request.json['email']
+            username = username.strip()
+            email = email.strip()
 
-@app.route('/debts')
-def get_debts():
-    debts = Debt.query.all()
-    return json.dumps([ debt.dictify() for debt in debts ])
-    # return json.dumps({'debts': [ debt.dictify() for debt in debts ] })
+            if len(username) == 0 or len(email) == 0:
+                raise Exception("Failed, username empty.")
 
-@app.route('/user/<user_id>')
+            conflicts = User.query.filter_by(username=username).all()
+            if len(conflicts) > 0:
+                raise Exception("Failed, username already taken.")
+
+            conflicts = User.query.filter_by(email=email).all()
+            if len(conflicts) > 0:
+                raise Exception("Failed, email already taken.")
+
+            new_user = User(username, email)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return json.dumps({'id': new_user.id})
+        except Exception as e:
+            app.logger.error(e)
+            abort(500)
+    elif request.method == "GET":
+        users = User.query.all()
+        return json.dumps([ user.dictify() for user in users ] )
+
+@app.route('/debts', methods=['GET', 'POST'])
+def get_debts(group_id = None):
+    if request.method == 'GET':
+        debts = Debt.query.all()
+        return json.dumps([ debt.dictify() for debt in debts ])
+    elif request.method == 'POST':
+        lender = User.query.get(request.json['lender']['id'])
+        debtor = User.query.get(request.json['debtor']['id'])
+        print lender, debtor
+
+        if not(user_in_group(lender, group_id) and user_in_group(debtor, group_id)):
+            print "Users not in group", lender, debtor, group_id
+            abort(500)
+
+        if lender.id == debtor.id:
+            print "Users identical"
+            abort(500)
+
+        amount = request.json['amount']
+
+        if not amount > 0:
+            print "Amount not > 0"
+            abort(500)
+
+        description = request.json['description'].strip()[:300]
+
+        new_debt = Debt(debtor = debtor, lender = lender, amount = amount, description = description)
+        db.session.add(new_debt)
+        db.session.commit()
+
+        return json.dumps({'id': new_debt.id})
+
+
+@app.route('/debts/<debt_id>', methods=['GET', 'PUT', 'DELETE'])
+def update_debt(debt_id, group_id = None):
+    debt = Debt.query.get(debt_id)
+    if request.method == 'GET':
+        return debt.json()
+
+    elif request.method == 'PUT':
+        lender = User.query.get(request.json['lender']['id'])
+        debtor = User.query.get(request.json['debtor']['id'])
+
+        if not(user_in_group(lender, group_id) and user_in_group(debtor, group_id)):
+            print "Users not in group", lender, debtor, group_id
+            abort(500)
+
+        if lender.id == debtor.id:
+            print "Users identical"
+            abort(500)
+
+        amount = request.json['amount']
+
+        if not amount > 0:
+            print "Amount not > 0"
+            abort(500)
+
+        description = request.json['description'].strip()[:300]
+
+        debt.debtor = debtor
+        debt.lender = lender
+        debt.amount = amount
+        debt.description = description
+        debt.paid = request.json['paid']
+
+        db.session.commit()
+
+        return ""
+
+    elif request.method == 'DELETE':
+        print "Implement access control"
+        db.session.delete(debt)
+        db.session.commit()
+        return ""
+
+def user_in_group(user, group):
+    return True
+
+@app.route('/users/<user_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_user(user_id):
     user = User.query.get(user_id)
-    return user.json()
+    if request.method == 'GET':
+        return user.json()
+    elif request.method == 'PUT':
+        email = request.json['email']
+        email.strip()
+        user.email = email
+        db.session.commit()
+        return user.json()
+    elif request.method == 'DELETE':
+        print "Authentication for user deletion not yet implemented"
+        abort(500)
+
 
 @app.route('/user/<user_id>/debts')
 def get_user_debts(user_id):
@@ -35,7 +144,7 @@ def get_user_loans(user_id):
     loans = User.query.get(user_id).loans
     return json.dumps({'loans': [ loan.dictify() for loan in loans ]})
 
-@app.route('/create/user', methods=['POST'])
+# @app.route('/create/user', methods=['POST'])
 def create_user():
     try:
         username = request.form['username']
@@ -87,7 +196,7 @@ def create_debt():
         app.logger.error(e)
         abort(500)
 
-@app.errorhandler(404)
+# @app.errorhandler(404)
 @app.errorhandler(500)
 def fail(error):
     colours = {404: "#036", 500: "#900"}
